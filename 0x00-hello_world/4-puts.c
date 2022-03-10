f077f4d4ecad9c8dc872ce3badaa71bce248c3c6
Temitayo Olusola <emmanuelolusola315@gmail.com>
2022-03-10 15:22:30 -0800

    Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+ * main - would carry the function for the phrase 'Programming is like building a multilingual puzzle
+ * Return 0, if stopped properly, else non-zero
+ */
+int main()
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
