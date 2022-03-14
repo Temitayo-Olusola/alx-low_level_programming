6467c77ffa1a9794cd694e4047a20afc31adb158
Temitayo Olusola <emmanuelolusola315@gmail.com>
2022-03-13 19:26:57 -0700

    Number

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,17 +3,16 @@
 /**
 *main - prints all single digit numbers of base 10
 *followed by a new line, using putchar
-*Return:  Always 0 (Success)
+*Return: Always 0 (Success)
 */
 int main(void)
 {
 	int n;
-	
-	for (n = 48; n < 58; n++)
+
+	for (n = 48; n <58; n++)
 	{
 		putchar(n);
 	}
-	putchar('\n');
-
+	putchar('\n')
 return (0);
 }
