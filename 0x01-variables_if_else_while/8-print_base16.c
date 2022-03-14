e7de11a7367d536f332634b3e275eb9e4f2689f9
Temitayo Olusola <emmanuelolusola315@gmail.com>
2022-03-13 18:26:05 -0700

    Hexas

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -9,14 +9,14 @@ int main(void)
 {
 		int n;
 		char ch;
-		
+
 		for (n = 48; n < 58; n++)
 		{
-		       	putchar(n);
+			putchar(n);
 		}
 		for (ch = 'a'; ch <= 'f'; ch++)
 		{
-			putchar(ch);
+			putchar(n);
 		}
 		putchar('\n');
 return (0);
