1617506e4fac01dbd628c31549f834eb3ce484d4
Temitayo Olusola <emmanuelolusola315@gmail.com>
2022-03-15 18:48:31 -0700

    98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -24,9 +24,9 @@ void print_to_98(int n)
 	{
 		for (j = n; j >= 98; j--);
 		{
-			if(j != 98)
+			if (j != 98)
 				printf("%d, ", j);
-			else if(j == 98)
+			else if (j == 98)
 				printf("%d\n", j);
 		}
 	}
