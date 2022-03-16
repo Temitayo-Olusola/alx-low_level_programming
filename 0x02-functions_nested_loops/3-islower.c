93419903729dec1dd9885729f74a493fd45fb41d
Temitayo Olusola <emmanuelolusola315@gmail.com>
2022-03-15 17:38:40 -0700

    islower

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _islower - checks for lowercase character 
+ * _islower - checks for lowercase character
  * @c: the charater to check
  * Return: 1 if c is lowercase, 0 if otherwise
  */
