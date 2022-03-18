15222d8f06c6a6efac333979186b0d7bb41310f4
Temitayo Olusola <emmanuelolusola315@gmail.com>
2022-03-17 17:17:44 -0700

    upper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,8 +3,8 @@
 /**
  * _isupper - checks for uppercase characters
  * @c: the character to be checked
- * Return: 1 if c is uppercase, 0 otherwise */
-
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
 int _isupper(int c)
 {
 	return (c >= 'A' && c <= 'Z');
