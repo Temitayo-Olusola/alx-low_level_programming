ca8d08c0c2aa01fc161dbe6ec4502a4b3f2457d1
Temitayo Olusola <emmanuelolusola315@gmail.com>
2022-03-21 16:35:18 -0700

    001

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_H_
+#define _MAIN_H_
 
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
@@ -10,6 +10,6 @@ void rev_string(char *s);
 void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int *b);
-char *strcpy(char *dest, char *src);
+char *_strcpy(char *dest, char *src);
 
-#endif /* MAIN_H
+#endif 
