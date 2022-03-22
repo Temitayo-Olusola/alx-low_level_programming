590f77970863514e20818a16879424dd7c25d4b3
Temitayo Olusola <emmanuelolusola315@gmail.com>
2022-03-21 17:44:07 -0700

    009

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -12,11 +12,12 @@ void print_array(int *a, int n)
 {
 	int i = 0;
 
-	for (; i < n; i++)
+	for (i = 0; i < n;)
 	{
-		printf("%d", *(a+1));
-		if (i != (n - 1))
-			printf(", ");
+		if (i == 0)
+			printf("%d", a[i]);
+		else
+			printf(", %d", a[i]);
 	}
 	printf("\n");
 }
