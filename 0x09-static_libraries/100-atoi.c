eb8f322e76523905ffdbd8d5a7af40b605160783
Temitayo Olusola <emmanuelolusola315@gmail.com>
2022-04-01 18:42:38 -0700

    Static Libraries!

--- 0x09-static_libraries/100-atoi.c
+++ 0x09-static_libraries/100-atoi.c
@@ -15,12 +15,14 @@ int _atoi(char *s)
 	n = 0;
 	len = 0;
 	f = 0;
-	digit   = 0;
+	digit = 0;
 	while (s[len] != '\0')
 		len++;
 	while (i < len && f == 0)
 	{
-		if (s[i] == '-' && s[i] <= '9')
+		if (s[i] == '-')
+			++d;
+		if (s[i] >= '0' && s[i] <= '9')
 		{
 			digit = s[i] - '0';
 			if (d % 2)
@@ -33,7 +35,10 @@ int _atoi(char *s)
 		}
 		i++;
 	}
+
 	if (f == 0)
 		return (0);
+
 	return (n);
+}
 }
