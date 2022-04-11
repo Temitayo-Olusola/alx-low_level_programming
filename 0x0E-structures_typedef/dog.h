530eb7abd4a4636064dd289a126b3c2935b892a3
Temitayo Olusola <emmanuelolusola315@gmail.com>
2022-04-11 15:58:44 -0700

    000

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,7 +16,6 @@ struct dog
 }
 /**
  * dog_t - typedef for struct dog
- */
 typedef struct dog dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
@@ -25,3 +24,4 @@ int _strlen(char *s);
 void free_dog(dog_t *d);
 dog_t *new_dog(char *name, float age, char *owner);
 #endif
+*/
