06ca614023ce7e83bb292c334483df69b5c7198c
Temitayo Olusola <emmanuelolusola315@gmail.com>
2022-04-25 12:38:36 -0700

    003

--- 0x13-more_singly_linked_lists/2-add_noteint.c
+++ 0x13-more_singly_linked_lists/2-add_noteint.c
@@ -1,12 +1,12 @@
 #include "lists.h"
 /**
- * *add_noteint - adds a new node at the beginning of a linked list
+ * add_noteint - adds a new node at the beginning of a linked list
  * @head: pointer to the first nodee in the list
  * @n: data to insert in that new mode
  *
  * Return: pointer to the new node, or NULL if it fails
  */
-listint_t *add_nodeint(listint_t **head, const int n)
+listint_t add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
 
